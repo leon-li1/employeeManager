bbb4390fffc163eba411212d2b5dc84481d4f1c5
Leon Li <leonli369@gmail.com>
2021-04-18 15:22:09 -0400

    done backend

--- src/main/java/com/example/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/com/example/employeemanager/repo/EmployeeRepo.java
@@ -3,9 +3,12 @@ package com.example.employeemanager.repo;
 import com.example.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+    @Transactional
     void deleteEmployeeById(Long id);
 
     Optional<Employee> findEmployeeById(Long id);
